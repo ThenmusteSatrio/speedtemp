95ef3814504d88ff7ec93af2455aab141049a068
ThenmusteSatrio <gilangsatrio218@gmail.com>
2024-10-20 19:25:53 +0700

    Initial commit

---
+++ src/PackageServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Vendor\SpeedTemp;
+
+use Illuminate\Support\ServiceProvider;
+
+class PackageServiceProvider extends ServiceProvider
+{
+
+    public function boot()
+    {
+        // Load migrations
+        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+
+        // Load views
+        $this->loadViewsFrom(__DIR__ . '/resources/views', 'nama-paket');
+
+        // Publish assets
+        $this->publishes([
+            __DIR__ . '/database/migrations/' => database_path('migrations'),
+            __DIR__ . '/resources/views' => resource_path('views/vendor/speedtemp'),
+            __DIR__.'/path/to/migrations' => database_path('migrations'),
+        ]);
+    }
+
+    public function register()
+    {
+        //
+    }
+}
